87b92ea64865ca9390e70a5c0db630b50f8c0070
LucasHARosa <l.ucas.henrique.a.r@gmail.com>
2023-10-23 15:29:20 -0300

    atualização url

--- Mobile/src/services/api.ts
+++ Mobile/src/services/api.ts
@@ -15,7 +15,7 @@ type APIInstanceProps = AxiosInstance & {
 }
 
 const api = axios.create({
-  baseURL: 'http://192.168.100.30:3333',
+  baseURL: 'http://10.61.30.110:3333',
 }) as APIInstanceProps;
 
 let failedQueued: Array<PromiseType> = [];
